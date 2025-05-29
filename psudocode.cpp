1c225b212602b2b53dd537ae6e6c122eb9afd7d8
Jiwoong Choi <65582400+chlwldnd3643@users.noreply.github.com>
2025-05-29 14:17:57 +0900

    Update psudocode.cpp
    simple Initialzation updated

--- psudocode.cpp
+++ psudocode.cpp
@@ -1,3 +1,24 @@
+//PSEDOCODE EDITING!!
+
+
+initial_on(){
+    
+//hydrogen part initialization
+solenoid_hydrogenblocking=1; //1means allowed, 0 means blocking
+solenoid_hydrogenout=1;
+recirculation_blower=1;
+
+//Oxygen Part initialization
+Compressor = 1;
+Solenoid_O2 = 1;
+
+//Coolant Part Initialization
+    
+}
+
+
+//coolant part hello
+while(1){
 if tmp < 70:
 
     cod heater =1
@@ -15,3 +36,4 @@ if tmp>80:
     cod heater =0
 
     cbv -> loop1
+}
